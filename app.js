d893e4dd1577f80d760f469f9cfabc5b739cdae7
aaron <aaron@joshs-mbp.attlocal.net>
2021-08-19 10:39:49 -0700

    updated base setup

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const middleware = require("./lib/middleware");
-const express = require("express");
+const middleware = require('./lib/middleware');
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(middleware.requestLogger);
 
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
