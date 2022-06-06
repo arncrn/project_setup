d9bf57b463c265ab3754b82822f42035fd18082b
Nykaela Dodson <nykaeladodson@Nykaelas-MBP.attlocal.net>
2022-06-06 12:54:38 -0700

    added new commits

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const middleware = require('./lib/middleware');
 const express = require('express');
 const app = express();
+const fetch = require('isomorphic-fetch');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(middleware.requestLogger);
 
+function sendRequest(req, res) {
+  res.send('<h1>Hello World!</h1>');
+}
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/inventory/:item', async (req, res) => {
+  const { itemName } = req.params;
+
+  const response = await fetch(`http://www.recipepuppy.com/api/?i=${itemName}`);
+  const { title, href, results: recipes } = await response.json(); // not parsing correctly
+  const inventoryItem = { itemName: "eggs", quantity: 3 }; // hard coding db result
+
+  req.body = {
+    ...inventoryItem,
+    info: `Data obtained from ${title} - ${href}`,
+    recipes 
+  };
+
+  console.log(req.body); // logging this we can see that the values for info and recipes are undefined
 });
 
 app.use(middleware.unknownEndpoint);
